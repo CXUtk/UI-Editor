2ddefbc59b95cf70a445899bc8d10ea11bf71ae1
CXUtk <bdfzchen@163.com>
2020-05-01 10:53:10 -0400

    更加智能的文本框光标

--- UILib/Components/UITextBox.cs
+++ UILib/Components/UITextBox.cs
@@ -22,27 +22,23 @@ namespace UIEditor.UILib.Components {
     public class UITextBox : UIElement {
         private UILabel _label;
         private string _text;
-        private int caret;
+        private int _carrot;
         private float _timer;
         private bool _shouldBlink;
-        private float _offsetX;
+        private float _offsetR, _offsetL;
 
         public Texture2D FrameTexture { get; set; }
         public string Text {
-            get
-            {
+            get {
                 return _text;
             }
-            set
-            {
-                if (_text != value)
-                {
+            set {
+                if (_text != value) {
                     var e = new UITextChangeEvent(_text, value, this, Main._drawInterfaceGameTime.TotalGameTime);
                     TextChange(e);
-                    if (!e.Cancel)
-                    {
+                    if (!e.Cancel) {
                         _text = e.NewString;
-                        caret = _text.Length;
+                        _carrot = _text.Length;
                     }
                 }
             }
@@ -68,6 +64,7 @@ namespace UIEditor.UILib.Components {
                 Pivot = new Vector2(0, 0.5f),
                 NoEvent = true,
             };
+            _offsetL = _offsetR = 0;
             AppendChild(_label);
         }
         public override void FocusOn(UIActionEvent e) {
@@ -78,32 +75,23 @@ namespace UIEditor.UILib.Components {
         public override void UnFocus(UIActionEvent e) {
             _shouldBlink = false;
             _timer = 0;
-            caret = Text.Length;
+            // Carrot = Text.Length;
             base.UnFocus(e);
         }
         public override void UpdateSelf(GameTime gameTime) {
             base.UpdateSelf(gameTime);
-            caret = (int)MathHelper.Clamp(caret, 0, Text.Length);
-            if (IsFocused)
-            {
+            _carrot = (int)MathHelper.Clamp(_carrot, 0, Text.Length);
+            if (IsFocused) {
                 _timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
                 // 显示文本光标，500ms闪烁一次
-                if (_timer > 500)
-                {
+                if (_timer > 500) {
                     _shouldBlink ^= true;
                     _timer = 0;
                 }
-                _label.Text = Text.Insert(caret, _shouldBlink ? "|" : " ");
+                _label.Text = Text.Insert(_carrot, _shouldBlink ? "|" : " ");
                 _label.CalculateSize();
-                //// 5像素的偏移是留给光标的
-                //_offsetX = Math.Min(0, Width - 5f - _label.Width);
-                //_label.Position = new Vector2(_offsetX, 0);
-                _label.Recalculate();
-            }
-            else
-            {
-                if (_label.Text.Length != Text.Length)
-                {
+            } else {
+                if (_label.Text.Length != Text.Length) {
                     _label.Text = Text;
                 }
             }
@@ -113,6 +101,18 @@ namespace UIEditor.UILib.Components {
         public override void DrawSelf(SpriteBatch sb) {
             if (IsFocused) {
                 InputText();
+
+                // 5像素的偏移是留给光标的
+                float carrotpos = _label.MeasureSize(Text.Substring(0, _carrot)).X;
+                if (carrotpos >= _offsetR - 10f * TextScale) {
+                    _offsetR = carrotpos + 10f * TextScale;
+                    _offsetL = _offsetR - Width;
+                } else if (carrotpos <= _offsetL + 5f * TextScale) {
+                    _offsetL = carrotpos - 5f * TextScale;
+                    _offsetR = _offsetL + Width;
+                }
+                _label.Position = new Vector2(Math.Min(0, Width - _offsetR), 0);
+                _label.Recalculate();
                 DrawIME();
             }
             base.DrawSelf(sb);
@@ -123,52 +123,35 @@ namespace UIEditor.UILib.Components {
             OnTextChange?.Invoke(e, this);
         }
 
-        private void InputText()
-        {
+        private void InputText() {
             PlayerInput.WritingText = true;
             Main.instance.HandleIME();
             #region KeyDown
-            bool KeyDown(Keys key)
-            {
+            bool KeyDown(Keys key) {
                 return Main.keyState.IsKeyDown(key) && !Main.oldKeyState.IsKeyDown(key);
             }
             #endregion
-            if (KeyDown(Keys.End))
-            {
-                caret = Text.Length;
-            }
-            else if (KeyDown(Keys.Home))
-            {
-                caret = 0;
-            }
-            else if (KeyDown(Keys.LeftControl) && KeyDown(Keys.X))
-            {
+            if (KeyDown(Keys.End)) {
+                _carrot = Text.Length;
+            } else if (KeyDown(Keys.Home)) {
+                _carrot = 0;
+            } else if (KeyDown(Keys.LeftControl) && KeyDown(Keys.X)) {
                 Platform.Current.Clipboard = Text;
                 Text = string.Empty;
-            }
-            else if (KeyDown(Keys.Left))
-            {
-                caret = Math.Max(0, caret - 1);
-            }
-            else if (KeyDown(Keys.Right))
-            {
-                caret = Math.Min(Text.Length, caret + 1);
-            }
-            else
-            {
-                if (caret == Text.Length)
-                {
+            } else if (KeyDown(Keys.Left)) {
+                _carrot = Math.Max(0, _carrot - 1);
+            } else if (KeyDown(Keys.Right)) {
+                _carrot = Math.Min(Text.Length, _carrot + 1);
+            } else {
+                if (_carrot == Text.Length) {
                     Text = Main.GetInputText(Text);
-                }
-                else
-                {
-                    var front = Text.Substring(0, caret);
-                    var back = Text.Substring(caret);
+                } else {
+                    var front = Text.Substring(0, _carrot);
+                    var back = Text.Substring(_carrot);
                     var newString = Main.GetInputText(front);
-                    if (front != newString)
-                    {
+                    if (front != newString) {
                         Text = newString + back;
-                        caret = newString.Length;
+                        _carrot = newString.Length;
                     }
                 }
             }
